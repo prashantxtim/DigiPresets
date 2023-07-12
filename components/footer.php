7065167783eeb2dc8cd62151848ecabc90cfcc1f
prashantxtim <47918831+prashantxtim@users.noreply.github.com>
2023-07-12 12:05:34 +0545

    components

---
+++ components/footer.php
@@ -0,0 +1,27 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/whitelogo.png" alt="">
+         <p>Digi Presets is a versatile collection of professional-grade presets designed for digital photographers and content creators. With a wide range of styles and adjustments.</p>
+      </div>
+
+      <div class="box">
+         <h3>Contact Us</h3>
+         <a href="tel:1234567890"><i class="fas fa-phone"></i> +977 9840358372</a>
+         <a href="mailto:prashantxtim@gmail.com"><i class="fas fa-envelope"></i> prashantxtim@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Koteshwor </a>
+      </div>
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> All Products</a>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Signin</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Sign Up</a>
+      </div>
+
+
+
+   </section>
+</footer>
